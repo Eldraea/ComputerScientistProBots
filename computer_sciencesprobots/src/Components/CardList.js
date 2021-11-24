b963eba16d74c0d7a61f2649fd24117502ba549e
Kimberly <kjacques709@gmail.com>
2021-11-24 15:32:13 +0100

    First version ComputerSciencesProBots

---
+++ computer_sciencesprobots/src/Components/CardList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./Card";
+
+
+const CardList = ({robots}) =>
+{
+    
+    const cardComponent = robots.map((robot,i) => 
+        {
+            return(
+                <Card key={i} id={robots[i].id} firstName = {robots[i].firstName} lastName = {robots[i].lastName} profession={robots[i].profession}/>
+            );
+        })
+    return(
+        <div className ='tc'>
+            {cardComponent}
+        </div>
+    );
+
+}
+
+export default CardList;
